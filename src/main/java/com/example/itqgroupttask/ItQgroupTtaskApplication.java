99a09e62cb373db6e09f2962c01558bc207636f4
xomyakre <aLexsev95n@gmail.com>
2026-02-17 20:34:13 +0300

    add: github

---
+++ src/main/java/com/example/itqgroupttask/ItQgroupTtaskApplication.java
@@ -0,0 +1,14 @@
+package com.example.itqgroupttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItQgroupTtaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItQgroupTtaskApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/example/itqgroupttask/ItQgroupTtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itqgroupttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItQgroupTtaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
